2ce4855f25c5542d563f624fd2fe08ba717212f2
Cole Vertikoff <cole.vertikoff@gmail.com>
2018-04-18 12:28:24 -0400

    Converts uploaded image into base64 string

--- image-process-front-end/src/fileDrop.js
+++ image-process-front-end/src/fileDrop.js
@@ -8,10 +8,24 @@ class Basic extends React.Component {
   }
 
   onDrop(files) {
-    this.setState({
-      files
-    });
+    console.log(files.length)
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      file["base64"] = event.target.result;
+      this.setState({
+        files
+      });
+      console.log(this.state);
+    };
+
+    // Loop through all uploaded files
+    for(var i=0; i < files.length; i++){
+      var file = files[i]
+      reader.readAsDataURL(file);
+    }
   }
+
 
   render() {
     return (
@@ -25,7 +39,7 @@ class Basic extends React.Component {
           <h2>Dropped files</h2>
           <ul>
             {
-              this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+              this.state.files.map(f => <li key={f.name}><img src={f.base64}></img><br/>{f.name} - {f.size} bytes</li>)
             }
           </ul>
         </aside>
